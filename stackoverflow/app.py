7ae0dc40eaab3fd825779b3bba6e2decbe2dfee2
Smith Ringtho <sringtho@gmail.com>
2022-05-09 18:53:50 +0300

    Added endpoint for adding a question

--- stackoverflow/app.py
+++ stackoverflow/app.py
@@ -19,6 +19,24 @@ def get_questions():
     return jsonify({"questions": questions})
 
 #post a question
+@app.route('/questions', methods=["POST"])
+def add_question():
+    data = request.get_json()
+    question = {
+        "id": data['id'],
+        "question": data['question'],
+        "description": data['description'],
+        "stack": data['stack'],
+        "answers": []
+    }
+    qn = next(filter(lambda x: x['id'] == data['id'], questions), None)
+    if qn:
+        return jsonify({"error": f"The id {data['id']} already exists!"}), 400
+    questions.append(question)
+    return question, 201
+
+
+
 
 #get a particular question
 
@@ -26,5 +44,5 @@ def get_questions():
 
 #delete a particular question
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
