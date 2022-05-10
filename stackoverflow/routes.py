e0c95e53013aff041decf3105c48b3a8747ab69d
Smith Ringtho <sringtho@gmail.com>
2022-05-10 15:16:22 +0300

    Added author functionality to the questions endpoints

--- stackoverflow/routes.py
+++ stackoverflow/routes.py
@@ -15,7 +15,8 @@ questions = [
         "question": "Log in to use Flask",
         "description": "I would like to use Flask for authentication purpose",
         "stack":"Python, HTML, CSS",
-        "answers": []
+        "answers": [],
+        "author": "sringtho"
     }
 ]
 
@@ -59,8 +60,8 @@ def login():
         token = encode_token(user['id'], user['username'])
         return jsonify({
             "access_token": token,
-            "user_id": user["id"],
-            "username": user["username"]
+            "username": user["username"],
+            "email": user['email']
         })
     return jsonify({"error": "Incorrect password used"}), 400
 
@@ -93,54 +94,68 @@ def update_profile(username):
         return user
     return jsonify({"error" : "User not found"}), 404
 
-# @app.route('/questions')
-# def get_questions():
-#     return jsonify({"questions": questions})
+@app.route('/questions')
+def get_questions():
+    return jsonify({"questions": questions})
 
-# @app.route('/questions', methods=["POST"])
-# # @jwt_required()
-# def add_question():
-#     data = request.get_json()
-#     question = next(filter(lambda x: x['id'] == data['id'], questions), None)
-#     if question:
-#         return jsonify({"error": f"The id {data['id']} already exists!"}), 400
-#     new_question = {
-#         "id": data['id'],
-#         "question": data['question'],
-#         "description": data['description'],
-#         "stack": data['stack'],
-#         "answers": []
-#     }
-#     questions.append(new_question)
-#     return new_question, 201
+@app.route('/questions', methods=["POST"])
+@required_token
+def add_question():
+    current_user = get_current_user()
+    if current_user is None:
+        return jsonify({"error":"Please provide a token to continue"}), 401
+    data = request.get_json()
+    question = next(filter(lambda x: x['id'] == data['id'], questions), None)
+    if question:
+        return jsonify({"error": f"The id {data['id']} already exists!"}), 400
+    new_question = {
+        "id": data['id'],
+        "question": data['question'],
+        "description": data['description'],
+        "stack": data['stack'],
+        "answers": [],
+        "author": current_user['username']
+    }
+    questions.append(new_question)
+    return new_question, 201
 
-# @app.route('/questions/<int:id>')
-# def get_question(id):
-#     question = next(filter(lambda x: x['id'] == id, questions), None)
-#     if question:
-#         return question
-#     return jsonify({"error" : "Question not found"}), 404
+@app.route('/questions/<int:id>')
+def get_question(id):
+    question = next(filter(lambda x: x['id'] == id, questions), None)
+    if question:
+        return question
+    return jsonify({"error" : "Question not found"}), 404
 
 
-# @app.route('/questions/<int:id>', methods=["PUT"])
-# # @jwt_required()
-# def edit_question(id):
-#     data = request.get_json()
-#     question = next(filter(lambda x: x['id'] == id, questions), None)
-#     if question:
-#         question.update(data)
-#         return question
-#     return jsonify({"error" : "Question not found"})
+@app.route('/questions/<int:id>', methods=["PUT"])
+@required_token
+def edit_question(id):
+    current_user = get_current_user()
+    if current_user is None:
+        return jsonify({"error":"Please provide a token to continue"}), 401
+    data = request.get_json()
+    question = next(filter(lambda x: x['id'] == id, questions), None)
+    if question:
+        if current_user['username'] == question['author']:
+            question.update(data)
+            return question
+        return jsonify({"error": "You are unauthorized!"}), 401
+    return jsonify({"error" : "Question not found"})
 
-# @app.route('/questions/<int:id>', methods=["DELETE"])
-# # @jwt_required()
-# def delete_question(id):
-#     global questions
-#     question = next(filter(lambda x: x['id'] == id, questions), None)
-#     if not question:
-#         return jsonify({"error" : "Question not found"}), 404
-#     questions = list(filter(lambda x: x['id'] != id, questions))
-#     return jsonify({"message": "Question successfully deleted"})
+@app.route('/questions/<int:id>', methods=["DELETE"])
+@required_token
+def delete_question(id):
+    current_user = get_current_user()
+    if current_user is None:
+        return jsonify({"error":"Please provide a token to continue"}), 401
+    global questions
+    question = next(filter(lambda x: x['id'] == id, questions), None)
+    if not question:
+        return jsonify({"error" : "Question not found"}), 404
+    if current_user['username'] == question['author']:
+        questions = list(filter(lambda x: x['id'] != id, questions))
+        return jsonify({"message": "Question successfully deleted"})
+    return jsonify({"error": "You are unauthorized!"}), 401
 
 # @app.route('/questions/<int:id>/answers', methods=["POST"])
 # # @jwt_required()
