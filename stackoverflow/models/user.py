2a5f50a10e71869aadc5c541474362d704e269ac
Smith Ringtho <sringtho@gmail.com>
2022-05-15 11:23:26 +0300

    Update: Refactor user model code

--- stackoverflow/models/user.py
+++ stackoverflow/models/user.py
@@ -5,68 +5,51 @@ from psycopg2.extras import RealDictCursor
 class User:
 
     def create_user(self,username,email,firstname,lastname,gender,password):
-        conn = Database().get_db_connection()
-        cur = conn.cursor()
+        cur = Database().get_cursor()
         query =f"""
         INSERT INTO users (username,email,firstname,lastname,gender,password) 
         VALUES ('{username}','{email}','{firstname}','{lastname}','{gender}','{password}')
         """
         user = cur.execute(query)
-        conn.commit()
-        cur.close()
-        conn.close()
         return user
 
     def get_user_auth_details(self, username, password):
-        conn = Database().get_db_connection()
-        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur = Database().get_cursor()
         query =f"""
         SELECT * FROM users WHERE username='{username}'
         """
         cur.execute(query)
         user = cur.fetchone()
-        cur.close()
-        conn.close()
         if user and check_password_hash(user['password'], password):
             return user
 
     def get_user(self, username):
-        conn = Database().get_db_connection()
-        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur = Database().get_cursor()
         query=f"""
         SELECT username,email,firstname,lastname,gender FROM users WHERE username='{username}'
         """
         cur.execute(query)
         user = cur.fetchone()
-        cur.close()
-        conn.close()
         return user
 
     def update_user_password(self, username, password):
-        conn = Database().get_db_connection()
-        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur = Database().get_cursor()
         query=f"""
         UPDATE users SET password='{password}' WHERE username='{username}'
         """
         cur.execute(query)
         rows = cur.rowcount
-        conn.commit()
-        cur.close()
-        conn.close()
         return rows
 
 
     def get_current_user_from_token(self):
+        cur = Database().get_cursor()
         username = get_username_from_token()
-        conn = Database().get_db_connection()
-        cur = conn.cursor(cursor_factory=RealDictCursor)
         query=f"""
         SELECT username FROM users WHERE username='{username}'
         """
         cur.execute(query)
         user = cur.fetchone()
-        cur.close()
-        conn.close()
         return user
 
 
